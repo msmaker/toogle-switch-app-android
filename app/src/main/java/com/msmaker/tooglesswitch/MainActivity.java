cf78b6339bad0a3bf7638366345f1af95c910816
Marco Silva <marcosilva.tec@gamil.com>
2021-07-06 13:13:13 -0300

    Listener toggle

--- app/src/main/java/com/msmaker/tooglesswitch/MainActivity.java
+++ app/src/main/java/com/msmaker/tooglesswitch/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void adcionarListener() {
-        switchSenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        toggleSenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
