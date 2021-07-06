b7dbbe193c2cb5cc636460704be46f3a0df7f764
Marco Silva <marcosilva.tec@gamil.com>
2021-07-06 13:04:42 -0300

    layout

---
+++ app/src/main/java/com/msmaker/tooglesswitch/MainActivity.java
@@ -0,0 +1,40 @@
+package com.msmaker.tooglesswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Switch switchSenha;
+    private ToggleButton toggleSenha;
+    private TextView textResultado;
+    private CheckBox checkBoxSenha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        switchSenha = findViewById(R.id.switchSenha);
+        toggleSenha = findViewById(R.id.toggleSenha);
+        textResultado = findViewById(R.id.textResultado);
+        checkBoxSenha = findViewById(R.id.checkBoxSenha);
+    }
+
+    public void enviar(View view) {
+
+        //if (toggleSenha.isChecked()) {
+        //if (switchSenha.isChecked()) {
+        if (checkBoxSenha.isChecked()) {
+            textResultado.setText("Check ligado");
+        } else {
+            textResultado.setText("Check desligado");
+        }
+    }
+}
